fccd8de731a5631f14f6fa92cb42ea22d1f1e7a2
Torsten Wetzel <torsten@orderlift.com>
2015-02-20 15:33:16 +0100

    new initializer

---
+++ lib/generators/magic_addresses/templates/magic_initializer.rb
@@ -0,0 +1,12 @@
+MagicAddresses.configure do |config|
+  # in which locales addresses should be saved
+  config.active_locales = [:en, :de]
+  # what is the default language (should be :en, except you don't need english at all)
+  config.default_locale = :en
+  # use a background-process for the lookups
+  config.job_backend = :none
+  # use a postgres earthdistance for distance calculation
+  config.earthdistance = false
+  # use a postgres hstore or serializer
+  config.hstore = false
+end
