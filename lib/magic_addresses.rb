5d6d648634cc47e3be6dea9a5a86fae7c7b3256f
Torsten Wetzel <torsten@orderlift.com>
2015-02-26 01:22:27 +0100

    move app folder into lib

--- lib/magic_addresses.rb
+++ lib/magic_addresses.rb
@@ -7,9 +7,9 @@ require 'geocoder/results/nominatim_decorator'
 
 require "magic_addresses/configuration"
 
-require "../../app/models/magic_addresses/association"
-require "../../app/models/magic_addresses/translator"
-require "../../app/models/magic_addresses"
+require "app/models/magic_addresses/association"
+require "app/models/magic_addresses/translator"
+require "app/models/magic_addresses"
 
 require "helpers/mgca_helper"
 
@@ -18,30 +18,30 @@ require "magic_addresses/railtie" if defined?(Rails::Railtie)
 module MagicAddresses
   
   # models
-  autoload :Address,      "../../app/models/magic_addresses/address"
-  autoload :Country,      "../../app/models/magic_addresses/country"
-  autoload :State,        "../../app/models/magic_addresses/state"
-  autoload :City,         "../../app/models/magic_addresses/city"
-  autoload :District,     "../../app/models/magic_addresses/district"
-  autoload :Subdistrict,  "../../app/models/magic_addresses/subdistrict"
+  autoload :Address,      "app/models/magic_addresses/address"
+  autoload :Country,      "app/models/magic_addresses/country"
+  autoload :State,        "app/models/magic_addresses/state"
+  autoload :City,         "app/models/magic_addresses/city"
+  autoload :District,     "app/models/magic_addresses/district"
+  autoload :Subdistrict,  "app/models/magic_addresses/subdistrict"
   
   # services
-  autoload :GeoCoder,     "../../app/models/magic_addresses/geo_coder"
+  autoload :GeoCoder,     "app/models/magic_addresses/geo_coder"
   
   # controllers
-  autoload :BaseController,           "../../app/controllers/magic_addresses/base_controller"
-  autoload :CountriesController,      "../../app/controllers/magic_addresses/countries_controller"
-  autoload :StatesController,         "../../app/controllers/magic_addresses/states_controller"
-  autoload :CitiesController,         "../../app/controllers/magic_addresses/cities_controller"
-  autoload :DistrictsController,      "../../app/controllers/magic_addresses/districts_controller"
-  autoload :SubdistrictsController,   "../../app/controllers/magic_addresses/subdistricts_controller"
+  autoload :BaseController,           "app/controllers/magic_addresses/base_controller"
+  autoload :CountriesController,      "app/controllers/magic_addresses/countries_controller"
+  autoload :StatesController,         "app/controllers/magic_addresses/states_controller"
+  autoload :CitiesController,         "app/controllers/magic_addresses/cities_controller"
+  autoload :DistrictsController,      "app/controllers/magic_addresses/districts_controller"
+  autoload :SubdistrictsController,   "app/controllers/magic_addresses/subdistricts_controller"
   
   class << self
     
     attr_accessor :configuration
 
     #initialize "mgca.add_views" do |app|
-    #  app.view_paths.insert 0, ActionView::FileSystemResolver.new( "../../app/views" )
+    #  app.view_paths.insert 0, ActionView::FileSystemResolver.new( "app/views" )
     #end
 
     # Call this method to modify defaults in your initailizers.
@@ -61,6 +61,10 @@ module MagicAddresses
     
   end
   
+  def self.root
+    File.expand_path '../..', __FILE__
+  end
+  
 end
 
 ActiveSupport.on_load(:active_record) do
@@ -68,7 +72,8 @@ ActiveSupport.on_load(:active_record) do
   require 'geocoder'
 end
 
-ActionController::Base.prepend_view_path File.dirname(__FILE__) + "../../app/views"
+# => ActionController::Base.prepend_view_path File.dirname(__FILE__) + "app/views"
+ActionController::Base.append_view_path File.dirname(__FILE__) + "app/views"
 
 #require 'geocoder'
 
