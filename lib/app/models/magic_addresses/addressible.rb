f2ed21c63108b3187989f56c2eae16e4d18db790
Torsten Wetzel <torsten@orderlift.com>
2016-01-21 13:16:16 +0100

    addressible params

--- lib/app/models/magic_addresses/addressible.rb
+++ lib/app/models/magic_addresses/addressible.rb
@@ -1,11 +1,19 @@
 # encoding: utf-8
 class MagicAddresses::Addressible < ActiveRecord::Base
   
+  # =====> C O N S T A N T S <=============================================================== #
+  PARAMS = [ :id, :owner_type, :owner_id, :address_attributes => MagicAddresses::Address::PARAMS, :_destroy ]
+  
+  
+  # =====> A S S O Z I A T I O N S <========================================================= #
   belongs_to :owner,      polymorphic: true
   belongs_to :address,    class_name: "MagicAddresses::Address",    foreign_key: :address_id
   
+  
+  # =====> C A L L B A C K S <=============================================================== #
   after_commit :log_some_stuff
   
+  # =====>  P  R  I  V  A  T  E  !  <======================================================== # # # # # # # #
 private
   
   def log_some_stuff
--- lib/magic_addresses/version.rb
+++ lib/magic_addresses/version.rb
@@ -1,3 +1,3 @@
 module MagicAddresses
-  VERSION = "0.0.39.rc4"
+  VERSION = "0.0.39.rc5"
 end
